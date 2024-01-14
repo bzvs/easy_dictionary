abe9192142ec467090fe8a7a2abd13ecebb24c14
Viacheslav Bezzubenko <vbezzubenko@mmtr.ru>
2024-01-14 13:33:14 +0300

    initial commit

---
+++ src/main/java/com/bzvs/easydict/EasydictApplication.java
@@ -0,0 +1,13 @@
+package com.bzvs.easydict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasydictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasydictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bzvs/easydict/EasydictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzvs.easydict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasydictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
