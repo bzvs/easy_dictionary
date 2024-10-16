102aad4c9b5b528057703bd249469b9f23f40625
Bezzubenko Viacheslav <bezzubenko_slava@mail.ru>
2024-10-16 15:34:21 +0300

    fix unauth bug

--- src/main/java/com/bzvs/easydict/service/UserServiceImpl.java
+++ src/main/java/com/bzvs/easydict/service/UserServiceImpl.java
@@ -5,7 +5,9 @@ import com.bzvs.easydict.entity.UserEntity;
 import com.bzvs.easydict.mapper.UserMapper;
 import com.bzvs.easydict.repository.api.UserRepository;
 import com.bzvs.easydict.service.api.UserService;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +20,14 @@ public class UserServiceImpl implements UserService {
     private final UserRepository repository;
     private final UserMapper mapper;
 
+    private static final String BAD_CREDENTIALS_MESSAGE = "Bad credentials or unregistered user";
+
     @Override
     public UserDto findByEmail(String email) {
         UserDto user = mapper.mapToDto(repository.findByEmail(email));
+        if (Objects.isNull(user)) {
+            throw new BadCredentialsException(BAD_CREDENTIALS_MESSAGE);
+        }
 
         return initializeDetailsFields(user);
     }
